dac1f11e566ca29c3bce70ecfbcec8be6fb9d771
Jim Tittsler <jim@oerfoundation.org>
2012-06-14 00:41:27 +0000

    tool for removing all of a service from db

---
+++ harvest/tools/remove_ask.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var fs = require('fs'),
+    http = require('http'),
+    couch = require('couch-client');
+
+var couchserver = JSON.parse(fs.readFileSync('couchserver.json', 'utf8'));
+var mentionsdb = couch(couchserver['url'] + '/' + couchserver['db']);
+
+var i;
+
+mentionsdb.view('/mentions/_design/ids/_view/ask', {},
+    function(err, doc) {
+      console.log('err', err);
+      console.log('doc', doc);
+      for (i = 0; i < doc.total_rows; i++) {
+        mentionsdb.remove(doc.rows[i].id);
+      }
+      console.log('all removed, wait for it to happen');
+    });
+
