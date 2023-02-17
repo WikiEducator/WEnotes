1ebd2dc84410eaaea36616b8125abea2426194af
Dave Lane <dave@davelane.nz>
2023-02-17 14:22:44 +1300

    adding Te Pūkenga course designation

--- WEnotesClient.js
+++ WEnotesClient.js
@@ -2866,6 +2866,8 @@ var msg_counter = [];
               coursesite = 'pacificcourse.col';
           } else if (d.we_source_url === 'course.milll.ws') {
 	      coursesite = 'course.milll';
+          } else if (d.we_source_url === 'tepukenga.oerfoundation.org') {
+	      coursesite = 'course.tepukenga';
     	  } else {
     	      coursesite = 'course.oeru';
           }
